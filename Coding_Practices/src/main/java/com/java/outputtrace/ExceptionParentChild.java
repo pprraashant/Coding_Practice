5886aa5badab03050a39b60c38b141525cee6b65
pprraashant <prashant2serve4u@gmail.com>
2019-03-05 10:35:32 +0530

    add class for parent child exception check

---
+++ Coding_Practices/src/main/java/com/java/outputtrace/ExceptionParentChild.java
@@ -0,0 +1,84 @@
+package com.java.outputtrace;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ExceptionParentChild {
+	
+	public void methodcheck() throws RuntimeException
+	{
+		int n= 0;
+		try
+		{
+		int result = 40/n;
+		
+		System.out.println(result);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("array index out of bound");
+		}
+		catch(ArithmeticException ae)
+		{
+			System.out.println("Arithmetic Exception");
+		}
+		
+		try
+		{
+			File j= new File("");
+			System.out.println(j.canRead());
+		
+		int result = 40/n;
+		
+		System.out.println(result);
+		}
+		catch(ArithmeticException ae)
+		{
+			System.out.println("Arithmetic Exception");
+		}
+		
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("array index out of bound");
+		}
+	}
+	
+	public static void main(String [] args) 
+	{
+		int n= args.length;
+		try
+		{
+		int result = 40/n;
+		
+		System.out.println(result);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("array index out of bound");
+		}
+		catch(ArithmeticException ae)
+		{
+			System.out.println("Arithmetic Exception");
+		}
+		
+		try
+		{
+		int result = 40/n;
+		
+		System.out.println(result);
+		}
+		catch(ArithmeticException ae)
+		{
+			System.out.println("Arithmetic Exception");
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("array index out of bound");
+		}
+		
+		ExceptionParentChild ex= new ExceptionParentChild();
+		ex.methodcheck();
+		
+	}
+
+}
