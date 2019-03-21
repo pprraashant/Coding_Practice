6b11444e038d9dda1b706855f491c4ffcfa6d5f8
Prashant Pawar (99073) <prashant.pawar1@amdocs.com>
2019-03-21 16:25:05 +0530

    csv to JSON conversion

--- Coding_Practices/src/main/java/com/java/json/JSONCreation.java
+++ Coding_Practices/src/main/java/com/java/json/JSONCreation.java
@@ -13,43 +13,87 @@ public class JSONCreation {
 	
 	public static void main(String [] args) throws IOException
 	{
+		doingsomting();
+	}
+
+	public static void doingsomting() throws IOException
+	{
 		String userdir=System.getProperty("user.dir");
         String csvFileName=userdir+"\\Files\\Tree.csv";
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(csvFileName)));
        String line="";
        int noofrows=0;
-        while ((line = reader.readLine()) != null) {
-        	
-        	String n=line.trim();
-        	String n1=line.replaceAll("\\s{2,}","").trim();
+       String namespacetrack="";
+       JSONObject json2 = new JSONObject();
+       JSONObject jsonoriginial = new JSONObject();
+        while ((line = reader.readLine()) != null) 
+        {
         	String [] nam=line.split("\\s{2,}");
-        	if(noofrows>1)
+        	if(noofrows>=1)
+        	{
+        		if(namespacetrack.equals(""))
+        			namespacetrack=nam[0];
+        			String namespace=nam[0];
+         		if(namespace.equals(namespacetrack))
+        		{
+        			
+        			json2=getPortsJson(nam,json2);
+        		
+        		}
+        		else
+        		{
+        			jsonoriginial.put(namespacetrack, json2);
+        			
+        			json2=new JSONObject();
+        			json2=getPortsJson(nam,json2);
+        			
+        		}
+        		namespacetrack=nam[0];
+        }
         	noofrows++;
         }
-       
   
-
+        jsonoriginial.put(namespacetrack, json2);
+        System.out.println("final json"+jsonoriginial);
 		
-		JSONObject json = new JSONObject();
-		json.put("port", "value");
-		JSONObject json2 = new JSONObject();
-		json2.put("Name", json);
-		JSONObject json22 = new JSONObject();
-		json22.put("Name", json);
-		
-		JSONObject json3 = new JSONObject();
-		json3.put("Namespace", json2);
-		
-		JSONObject json5= json3.getJSONObject("Namespace");
-		json5.put("Name2", json);
-		//json3.put("Name3", json2);
-		String newFileName=System.getProperty("user.dir")+"jasonfile"+".json";
+		String newFileName=System.getProperty("user.dir")+"\\Files\\jasonfile"+".json";
 		File f = new File(newFileName);
 		System.out.println(newFileName);
 		FileWriter fileWriter = new FileWriter(newFileName);
-		fileWriter.write(json3.toString());
+		fileWriter.write(jsonoriginial.toString());
 		fileWriter.flush();
-		
 	}
 
+	private static JSONObject getPortsJson(String[] nam, JSONObject json2) {
+		//String strports =nam[5].replaceAll("/TCP", "").replaceAll("/UDP", "");
+		String strports =nam[5];
+		JSONObject portsjson = new JSONObject();
+		//JSONObject json2 = new JSONObject();
+		if(strports.contains(","))
+		{
+			String multipleports[]=strports.split(",");
+			for(int i=0;i<multipleports.length;i++)
+			{
+				String[] ports= multipleports[i].split(":");
+    			if(ports.length==0 || ports.length==1)
+    			portsjson.put(multipleports[i], "");
+    			else
+    				portsjson.put(ports[0], ports[1]);
+			}
+			json2.put(nam[1], portsjson);
+		}
+		else
+		{
+		String[] ports= strports.split(":");
+		if(ports.length==0 || ports.length==1)
+			portsjson.put(strports, "");
+			else
+				portsjson.put(ports[0], ports[1]);
+		json2.put(nam[1], portsjson);
+		}
+		return json2;
+	}
+	
+
 }
+
